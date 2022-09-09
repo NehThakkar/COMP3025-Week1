c31aeb3fd0b8358d174f3cabc766525a94ccc9b7
Neh Thakkar <200458205@student.georgianc.on.ca>
2022-09-09 10:52:45 -0400

    Added Button to the View

--- app/src/main/java/ca/georgiancollege/lab1/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/lab1/MainActivity.kt
@@ -2,10 +2,16 @@ package ca.georgiancollege.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    // LifeCycle Function
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val HelloWorldLabel = findViewById(R.id.Hello_World_Label) as TextView
+
     }
 }
