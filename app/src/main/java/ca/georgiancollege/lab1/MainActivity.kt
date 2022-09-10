3a525ff035a90bf31f6e6eadab37c77f9070bc99
Neh Thakkar <200458205@student.georgianc.on.ca>
2022-09-10 19:58:13 -0400

    Completed Week 1 INE

--- app/src/main/java/ca/georgiancollege/lab1/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/lab1/MainActivity.kt
@@ -2,6 +2,7 @@ package ca.georgiancollege.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val HelloWorldLabel = findViewById(R.id.Hello_World_Label) as TextView
+        val ClickMeButton = findViewById(R.id.Click_Me_Button) as Button
 
+        ClickMeButton.setOnClickListener {
+            HelloWorldLabel.text = "Clicked!"
+        }
     }
 }
